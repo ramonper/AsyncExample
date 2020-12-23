2fc8324fac75d6b92b488c93dca1b5a3cf8a49d0
xGhost <46267645+ramonper@users.noreply.github.com>
2020-12-23 11:26:08 -0300

    Update Main.java

--- src/me/ramonper/asyncexample/Main.java
+++ src/me/ramonper/asyncexample/Main.java
@@ -19,7 +19,7 @@ public class Main {
         ConcurrentAsyncEmail.sendAsyncEmail(objectEmail); // Envia o email
         //send the e-mail
 
-        objectEmail.setRecipient("pauloricardo.jr1@gmail.com"); // Muda para um outro destinatário
+        objectEmail.setRecipient("ramonpg696@gmail.com"); // Muda para um outro destinatário
         // change to other e-mail
 
         ConcurrentAsyncEmail.sendAsyncEmail(objectEmail); // Envia outro email, para o novo destinatário.
