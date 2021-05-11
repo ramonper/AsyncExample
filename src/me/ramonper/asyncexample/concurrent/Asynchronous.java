505ea6c05e65325ddafbcfea5e2c00d15be3a6d8
Ramon <ramonpg696@gmail.com>
2021-05-11 16:07:16 -0300

    Delete Asynchronous.java

--- src/me/ramonper/asyncexample/concurrent/Asynchronous.java
+++
@@ -1,9 +0,0 @@
-package me.ramonper.asyncexample.concurrent;
-
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-
-public interface Asynchronous <P> {
-    ExecutorService threadPool = Executors.newFixedThreadPool(3);
-    public void sendAsync(P param);
-}
